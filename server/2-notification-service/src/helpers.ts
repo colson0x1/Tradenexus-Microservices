045b08a9cec2d2c10d5681830b6c6de9fedf9bb8
COLSON <colsoncalifornication@gmail.com>
2024-12-27 12:35:18 +0545

    feat (notification): configure email template + use etheral email client for testing

---
+++ server/2-notification-service/src/helpers.ts
@@ -0,0 +1,56 @@
+import path from 'path';
+
+import { IEmailLocals, winstonLogger } from '@colson0x1/tradenexus-shared';
+import { Logger } from 'winston';
+import { config } from '@notifications/config';
+import nodemailer, { Transporter } from 'nodemailer';
+import Email from 'email-templates';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'mailTransportHelper', 'debug');
+
+// `to` is receiver
+async function emailTemplatest(template: string, to: string, locals: IEmailLocals): Promise<void> {
+  try {
+    const smtpTransport: Transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: config.SENDER_EMAIL,
+        pass: config.SENDER_EMAIL_PASSWORD
+      }
+    });
+
+    const email: Email = new Email({
+      message: {
+        from: `TradeNexus App <${config.SENDER_EMAIL}>`
+      },
+      // For development, send is false but if we want to send an actual
+      // email, it should be set to true
+      send: true,
+      preview: false,
+      transport: smtpTransport,
+      views: {
+        options: {
+          extension: 'ejs'
+        }
+      },
+      // This `juice` is just for the email templates to be able to use our
+      // CSS, the CSS styles, the inline CSS styles. Because in our template
+      // engine, Im using inline CSS.
+      juice: true,
+      juiceResources: {
+        // If we're using `!important` flag in CSS, we want to preserve that.
+        preserveImportant: true,
+        webResources: {
+          // When we build our application, we need to specify the path
+          // In production, the outDir folder is going to be `/build`
+          relativeTo: path.join(__dirname, '../build')
+        }
+      }
+    });
+  } catch (error) {
+    // We don't need to send the `error` here because we're going to get a
+    // message inside our mail transport (`mail.transport.ts`)
+    log.error(error);
+  }
+}
