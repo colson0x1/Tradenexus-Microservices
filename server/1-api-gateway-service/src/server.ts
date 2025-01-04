a81bcb96eedd0ab5d623f05775073c41e6327758
COLSON <colsoncalifornication@gmail.com>
2025-01-04 18:02:18 +0545

    feat (api gateway): implement standard middleware and error handler

--- server/1-api-gateway-service/src/server.ts
+++ server/1-api-gateway-service/src/server.ts
@@ -1,10 +1,12 @@
-import { winstonLogger } from '@colson0x1/tradenexus-shared';
-import { Application } from 'express';
+import { CustomError, IErrorResponse, winstonLogger } from '@colson0x1/tradenexus-shared';
+import { Application, json, NextFunction, Request, Response, urlencoded } from 'express';
 import { Logger } from 'winston';
 import cookieSession from 'cookie-session';
 import cors from 'cors';
 import hpp from 'hpp';
 import helmet from 'helmet';
+import compression from 'compression';
+import { StatusCodes } from 'http-status-codes';
 
 const SERVER_PORT = 4000;
 const log: Logger = winstonLogger('', 'apiGatewatServer', 'debug');
@@ -21,7 +23,13 @@ export class GatewayServer {
     this.app = app;
   }
 
-  public start(): void {}
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware(this.app);
+    this.startElasticSearch();
+    this.errorHandler(this.app);
+  }
 
   private securityMiddleware(app: Application): void {
     // If we're using Express, then we need set this. Because if we don't
@@ -79,5 +87,60 @@ export class GatewayServer {
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
       })
     );
+  }
+
+  // @ middleware with the compressed JSON URL encoded
+  private standardMiddleware(app: Application): void {
+    // This will just be used to compress the request to make it the size
+    // lower or to reduce the size
+    app.use(compression());
+    // setting maximum size of our json request
+    app.use(json({ limit: '200mb' }));
+    // Because we're going to be passing data through request.body so we need
+    // to setup urlencoded cause without this, we won't be able to set up
+    // our request.body
+    // So with this middleware right below, when we send data through the body
+    // through a POST request, we can get it via request.body
+    app.use(urlencoded({ extended: true, limit: '200mb' }));
+  }
+
+  private routesMiddleware(app: Application): void {}
+
+  private startElasticSearch(): void {}
+
+  private errorHandler(app: Application): void {
+    // Check if maybe there is a request or a URL that doesn't exist and its
+    // used to send a request, then we need to catch that error
+    // So what this means is, if the user tries to use an endpoint that
+    // doesn't exist that is not defined in the server, then its going to
+    // throw an error right here
+    app.use('*', (req: Request, res: Response, next: NextFunction) => {
+      // But this is going to be a very unlikely scenario whereby the user
+      // will want to make a request with a URL that doesn't exist.
+      // Probably i'll only use this when developing to check endpoint that
+      // does not exist but once i go to production, this will be very difficult
+      // @ constructing a URL from a request
+      const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+      log.log('error', `${fullUrl} endpoint does not exist.`, '');
+      // send it back to the user
+      // This is going to be very unlikely once I moved to production except
+      // if a engineer i.e me makes a mistake
+      res.status(StatusCodes.NOT_FOUND).json({ message: 'The endpoint called does not exist.' });
+      next();
+    });
+
+    // Check for CustomError
+    app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+      log.log('error', `GatewayService ${error.comingFrom}:`, error);
+      // CustomError is the name of the abstract class and each of the error
+      // class is extending from CustomError
+      // If error object we get is of type CustomError i.e if the error instance
+      // is of type CustomError, then
+      if (error instanceof CustomError) {
+        res.status(error.statusCode).json(error.serializeErrors());
+      }
+      res.status(StatusCodes.NOT_FOUND).json({ message: 'The endpoint called does not exist.' });
+      next();
+    });
   }
 }
