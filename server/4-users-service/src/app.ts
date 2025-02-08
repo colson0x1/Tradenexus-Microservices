d6de894fba4b34395ce51a0874953fcfc56cb4bb
COLSON <colsoncalifornication@gmail.com>
2025-02-08 14:09:15 +0545

    feat(users): configure server

--- server/4-users-service/src/app.ts
+++ server/4-users-service/src/app.ts
@@ -1,9 +1,14 @@
 import { databaseConnection } from '@users/database';
 import { config } from '@users/config';
+import express, { Express } from 'express';
+import { start } from '@users/server';
 
 const initialize = (): void => {
   config.cloudinaryConfig();
   databaseConnection();
+  // Create an instance of Express
+  const app: Express = express();
+  start(app);
 };
 
 initialize();
