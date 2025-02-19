c5038a1bba4b09be255f3c9c652e82da5462f001
COLSON <colsoncalifornication@gmail.com>
2025-02-19 14:12:20 +0545

    feat(gig): configure docker file, editorconfig, env, eslint, npmrc, prettier, jest, tsconfig.json and package.json + add config for environment file and database connection

---
+++ server/5-gig-service/src/database.ts
@@ -0,0 +1,19 @@
+import { winstonLogger } from '@colson0x1/tradenexus-shared';
+import { Logger } from 'winston';
+import { config } from '@gig/config';
+import mongoose from 'mongoose';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'gigDatabaseServer', 'debug');
+
+// Create a connection
+const databaseConnection = async (): Promise<void> => {
+  try {
+    // Connect to the database
+    await mongoose.connect(`${config.DATABASE_URL}`);
+    log.info('Users service successfully connected to database.');
+  } catch (error) {
+    log.log('error', 'UsersService databaseConnection() method error:', error);
+  }
+};
+
+export { databaseConnection };
---
+++ server/5-gig-service/src/elasticsearch.ts
@@ -0,0 +1,29 @@
+import { Client } from '@elastic/elasticsearch';
+import { config } from '@gig/config';
+import { winstonLogger } from '@colson0x1/tradenexus-shared';
+import { Logger } from 'winston';
+import { ClusterHealthResponse } from '@elastic/elasticsearch/lib/api/types';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'gigElasticSearchServer', 'debug');
+
+const elasticSearchClient = new Client({
+  node: `${config.ELASTIC_SEARCH_URL}`
+});
+
+// `checkConnection` will be used to check the health status of our
+// Elasticsearch Node because we're running a single Node Cluster.
+const checkConnection = async (): Promise<void> => {
+  let isConnected = false;
+  while (!isConnected) {
+    try {
+      const health: ClusterHealthResponse = await elasticSearchClient.cluster.health({});
+      log.info(`GigService Elasticsearch health status - ${health.status}`);
+      isConnected = true;
+    } catch (error) {
+      log.error('Connection to Elasticsearch failed. Retrying...');
+      log.log('error', 'GigService checkConnection() method:', error);
+    }
+  }
+};
+
+export { checkConnection };
