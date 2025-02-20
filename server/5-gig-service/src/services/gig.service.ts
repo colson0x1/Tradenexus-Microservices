59ce2c3d880a6568cd61a11022ee5299844bfc9c
COLSON <colsoncalifornication@gmail.com>
2025-02-20 18:35:37 +0545

    feat(gig): implement gig service CREATE method

--- server/5-gig-service/src/services/gig.service.ts
+++ server/5-gig-service/src/services/gig.service.ts
@@ -2,7 +2,10 @@
 // MongoDB. So im only going to get the data i need from Elasticsearch.
 
 import { ISellerGig } from '@colson0x1/tradenexus-shared';
-import { getIndexedData } from '@gig/elasticsearch';
+import { addDataToIndex, getIndexedData } from '@gig/elasticsearch';
+import { GigModel } from '@gig/models/gig.schema';
+import { publishDirectMessage } from '@gig/queues/gig.producer';
+import { gigChannel } from '@gig/server';
 import { gigsSearchBySellerId } from '@gig/services/search.service';
 
 // This method will get the data from Elasticsearch
@@ -95,4 +98,99 @@ const getSellerPausedGigs = async (sellerId: string): Promise<ISellerGig[]> => {
   return resultHits;
 };
 
-export { getGigById, getSellerGigs, getSellerPausedGigs };
+/* @ Gig Service CREATE method */
+// I want to use this `addDataToIndex()` method thats defined on `src/elasticsearch.ts`
+// I've decided to implement in a way to first save it to MongoDB. We can also
+// decide that we want to remove MongoDB altogether like we dont want to use
+// mongoDB at all. If we decide to use that approach, then we'll have to generate
+// our `id` because this `_id` from elasticsearch is the id created by MongoDB.
+// That is what im using to index the document. So if we want to implement it
+// separately i.e i only will want to use Elasticsearch without MongoDB, then
+// we have to generate our own IDs. But for me, here, im saving it to MongoDB so
+// that i'll be able to use MongoDB. That is how i just decided to implement it.
+// Here, im going to return the normal gig object and then param that im passing
+// in is `gig` of type `ISellerGig` so im passing the complete `gig` object.
+const createGig = async (gig: ISellerGig): Promise<ISellerGig> => {
+  // Create the gig
+  // This `create` method, once it creates the object or the document in
+  // MongoDB, it will return new created document. So that is why im using
+  // `create` here.
+  const createdGig: ISellerGig = await GigModel.create(gig);
+  // As in the gig model i.e `gig.schema.ts`, i want to be able to use that
+  // transform. Because MongoDB create the `_id` and i dont need the `_id`
+  // property. So if i check this `createdGig` above, its going to have the
+  // `_id`. I dont need that because Elasticsearch will thrown an error.
+  // `_id` is a reserved keyword used by Elasticsearch. So what im going to do
+  // is, right here, im going to call `toJSON`. `toJSON()` is available inside
+  // this `createdGig` property.
+  // So this `toJSON` object will be added as a property into the every object
+  // once they are returned. So we have access to this `toJSON`
+  if (createdGig) {
+    // So im calling `toJSON` as a method. So what this means is,
+    // MongoDB has two types of documents. It has mongodb documents or rather
+    // mongoose documents because im using mongoose. So if i return `_doc`,
+    // then im going to have the mongoose document. And the mongoose document
+    // has some properties, a lot of properties that i dont need.
+    // And then, there's other type `rec`, which is a pure JavaScript JSON
+    // object. So first `_doc` is a mongoose object with different mongoose
+    // properties and our own properties. And then second one is the record (`rec`),
+    // that contains the pure JavaScript object notation which is the pure JSON
+    // object. And that is why im returning it in `toJSON` inside `gig.schema.ts`.
+    // So, once i call this `toJSON`, it'll return the pure JavaScript JSON
+    // object. But if we returned this `_doc`, then we are not getting the JSON
+    // object. We are getting the mongoose object instead.  But i dont need the
+    // mongoose object, i only need the JSON object. So that is why im returning
+    // this `rec` i.e record, right there.
+    // And here below, once i call this `toJSON` as a method, it'll return the
+    // `ISellerGig` object and not the mongoose object. So here now, inside
+    // this object i.e `data` im going to have virtual `id` property. I'll have
+    // this `id` and that `_id` one will already be deleted. So this `_id` is
+    // going to be deleted while im going to have this `id` property.
+    const data: ISellerGig = createdGig.toJSON?.() as ISellerGig;
+    // Here, i publish the message.
+    // Name of the exchange is in Users Service
+    // `users/src/queues/user.consumer.ts`
+    // So im going to send a message with a type `update-gig-count` with
+    // exchange and routing key to a particular queue and it will be consumed
+    // in the `user.consumer.ts` from the Users Service.
+    await publishDirectMessage(
+      gigChannel,
+      // exchange, routing key and type is on `user.consumer.ts` from Users Service.
+      // exchange name
+      'tradenexus-seller-update',
+      // routing key
+      'user-seller',
+      // Stringified JSON message
+      // What i want to do with this method is just to update the sellers gig
+      // count i.e `updateTotalGigsCount` as in `user.consumer.ts`. So if the
+      // seller creates a gig, i want to update this property `totalGigs` defined
+      // in `users/src/models/seller.schema.ts`.
+      // So if the seller creates the first gig, i increment that defauly value
+      // 0 by 1. If the user creates another gig, i increment it by 1 as well.
+      // So that is what i want to do with this direct message right here.
+      // So `type` is going to be `update-gig-count` as i pass the gig seller id
+      // and then also the count. count is going to be 1.
+      // gigSellerId is going to be id from `data.sellerId`
+      // `user.consumer.ts`
+      // So this is what im sending in the message
+      JSON.stringify({ type: 'update-gig-count', gigSellerId: `${data.sellerId}`, count: 1 }),
+      'Details sent to users service.'
+    );
+    // And then, i'll add the message to the Elasticsearch.
+    // `gigs` is the index, item id is the `_id` property thats retrieved
+    // right from Elasticsearch dev tools: GET gigs/_search
+    // that is what i want to add here so `createdGig._id` or `data.id`
+    // Both `createdGig._id` or `data.id` works. But i'll use `createdGig._id`
+    // and then last argument, i pass in the `data`.
+    await addDataToIndex('gigs', `${createdGig._id}`, data);
+  }
+  // So in nutshell, this function `createdGig`, i first create the gig i.e
+  // `createdGig`. And if the gig is returned, i convert the gig from the
+  // mongoose object to normal JSON object with this `toJSON` method. And then,
+  // i publish the message to the Users Service and then i add the data to
+  // Elasticsearch. So this is the method im going to use to add a new sellers
+  // gig to the index.
+  return createdGig;
+};
+
+export { getGigById, getSellerGigs, getSellerPausedGigs, createGig };
