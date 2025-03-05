aebf5b0d66e2b117b36b78e5a284cff6990725ca
COLSON <colsoncalifornication@gmail.com>
2025-03-05 19:19:05 +0545

    feat(gateway): update current user controller to use redis cache methods

--- server/1-api-gateway-service/src/controllers/auth/current-user.ts
+++ server/1-api-gateway-service/src/controllers/auth/current-user.ts
@@ -1,7 +1,12 @@
+import { GatewayCache } from '@gateway/redis/gateway.cache';
+import { socketIO } from '@gateway/server';
 import { authService } from '@gateway/services/api/auth.service';
 import { AxiosResponse } from 'axios';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+
+// Initialize an instance of Gateway Cache
+const gatewayCache: GatewayCache = new GatewayCache();
 
 export class CurrentUser {
   public async read(_req: Request, res: Response): Promise<void> {
@@ -12,5 +17,27 @@ export class CurrentUser {
   public async resendEmail(req: Request, res: Response): Promise<void> {
     const response: AxiosResponse = await authService.resendEmail(req.body);
     res.status(StatusCodes.OK).json({ message: response.data.message, user: response.data.user });
+  }
+
+  // Im going to add more methods here and the method is going to be called
+  // via API endpoints and they'll be used to retrieve logged in users and then
+  // remove logged in users as well.
+
+  public async getLoggedInUsers(_req: Request, res: Response): Promise<void> {
+    // The key `loggedInUsers` comes from gateway/src/sockets/socket.ts.
+    const response: string[] = await gatewayCache.getLoggedInUsersFromCache('loggedInUsers');
+    // Use socketIO instance that is exported form the server file
+    socketIO.emit('online', response);
+    // So when a particular user logs in, im going to make a request via the
+    // endpoint to call this `getLoggedInUsers` method and then i get all the
+    // users. So the users will be added and then i get all the users that are
+    // already logged in that are saved in the cache.
+    res.status(StatusCodes.OK).json({ message: 'User is online' });
+  }
+
+  public async removeLoggedInUser(req: Request, res: Response): Promise<void> {
+    const response: string[] = await gatewayCache.removeLoggedInUserFromCache('loggedInUsers', req.params.username);
+    socketIO.emit('online', response);
+    res.status(StatusCodes.OK).json({ message: 'User is offline' });
   }
 }
--- server/1-api-gateway-service/src/routes/current-user.ts
+++ server/1-api-gateway-service/src/routes/current-user.ts
@@ -13,11 +13,20 @@ class CurrentUserRoutes {
   public routes(): Router {
     // Using Prototype
     this.router.get('/auth/refresh-token/:username', authMiddleware.checkAuthentication, Refresh.prototype.token);
+    this.router.get('/auth/logged-in-user', authMiddleware.checkAuthentication, CurrentUser.prototype.getLoggedInUsers);
     // @ authMiddleware.checkAuthentication checks if currentUser exists. If
     // currentUser object does not exist, it throws an error.
     // authMiddleware.verifyUser is just to verify if the token is still valid.
     this.router.get('/auth/currentuser', authMiddleware.checkAuthentication, CurrentUser.prototype.read);
+
     this.router.post('/auth/resend-email', authMiddleware.checkAuthentication, CurrentUser.prototype.resendEmail);
+
+    // `/:username` because i need the username fro params right there on
+    // currentUser controller.
+    // So if this delete route is called, then its going to remove the user
+    // from the Redis cache. Probably when the user clicks on the logout button.
+    // And then they logged out. Then i want to remove them from the cache.
+    this.router.delete('/auth/logged-in-user/:username', authMiddleware.checkAuthentication, CurrentUser.prototype.getLoggedInUsers);
 
     return this.router;
   }
